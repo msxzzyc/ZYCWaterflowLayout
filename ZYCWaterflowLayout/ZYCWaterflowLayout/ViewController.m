f9ec30b15b8b5c78ca80722f47974c8b3ff9210c
msxzzyc <10168363742qq.com>
2017-08-08 01:17:22 +0800

    显示数据
    注意用MJExtension的objectArrayWithFilename:方法将plist文件中的数据转入模型数组。

--- ZYCWaterflowLayout/ZYCWaterflowLayout/ViewController.m
+++ ZYCWaterflowLayout/ZYCWaterflowLayout/ViewController.m
@@ -8,36 +8,91 @@
 
 #import "ViewController.h"
 #import "ZYCWaterflowLayout.h"
+#import "XMGShop.h"
+#import "XMGShopCell.h"
+#import "MJRefresh.h"
+#import "MJExtension.h"
 @interface ViewController ()<UICollectionViewDataSource,UICollectionViewDelegate>
-
+/** 所有的商品数据 */
+@property(nonatomic,strong)NSMutableArray *shops;
+@property(nonatomic,weak)UICollectionView *collectionView;
 @end
 @implementation ViewController
 static NSString *const shopId = @"shop";
+- (NSMutableArray *)shops
+{
+    if (!_shops) {
+        _shops = [NSMutableArray array];
+    }
+    return _shops;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setUpLayout];
+    [self setUpRefresh];
+}
+- (void)setUpRefresh
+{
+    self.collectionView.header = [MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewShops)];
+    
+    [self.collectionView.header beginRefreshing];
+    self.collectionView.footer = [MJRefreshAutoNormalFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreShops)];
+    self.collectionView.footer.hidden = YES;
+}
+- (void)loadNewShops
+{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSArray *shops = [XMGShop objectArrayWithFilename:@"1.plist"];
+        [self.shops removeAllObjects];
+        [self.shops addObjectsFromArray:shops];
+        
+        //刷新数据
+        [self.collectionView reloadData];
+        [self.collectionView.header endRefreshing];
+    });
+}
+- (void)loadMoreShops
+{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSArray *shops = [XMGShop objectArrayWithFilename:@"1.plist"];
+        [self.shops addObjectsFromArray:shops];
+        
+        //刷新数据
+        [self.collectionView reloadData];
+        [self.collectionView.footer endRefreshing];
+    });
+}
+- (void)setUpLayout
+{
     //创建布局
     UICollectionViewLayout *layout = [[ZYCWaterflowLayout alloc]init];
     //创建collentionView
     UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:self.view.bounds collectionViewLayout:layout];
-    [self.view addSubview:collectionView];
+    collectionView.backgroundColor = [UIColor whiteColor];
     collectionView.dataSource = self;
+    [self.view addSubview:collectionView];
+    self.collectionView = collectionView;
+    
     //注册
-    [collectionView registerClass:[UICollectionViewCell class]forCellWithReuseIdentifier:shopId];
+    [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([XMGShopCell class]) bundle:nil] forCellWithReuseIdentifier:shopId];
+    collectionView.dataSource = self;
 }
-
 
 #pragma mark - UICollectionViewDataSource
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
+    
     return 1;
 }
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 20;
+    self.collectionView.footer.hidden = self.shops.count == 0;
+    return self.shops.count;
 }
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:shopId forIndexPath:indexPath];
+    XMGShopCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:shopId forIndexPath:indexPath];
+    cell.shop = self.shops[indexPath.item];
     cell.backgroundColor = [UIColor orangeColor];
     return cell;
 }
